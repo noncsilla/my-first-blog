7b3c218745e1a315fa8c1084e3cee0e162cbe7f3
NonCsilla <noemi.cs.nemeth@gmail.com>
2015-11-28 16:58:48 +0100

    publis, delete, order

--- blog/views.py
+++ blog/views.py
@@ -7,24 +7,23 @@ from django.shortcuts import redirect
 from .forms import PostForm
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html',{'posts': posts})
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-
-    return render(request, 'blog/post_edit.html', {'form': form})
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('blog.views.post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
 		post = get_object_or_404(Post, pk=pk)
@@ -39,3 +38,17 @@ def post_edit(request, pk):
 		else:
 			form = PostForm(instance=post)
 		return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('blog.views.post_list')
