33e9dc86ed17aff7aa346d18ac3a3ee20f23efc7
NonCsilla <noemi.cs.nemeth@gmail.com>
2015-11-30 23:44:52 +0100

    comment for everyone

--- blog/views.py
+++ blog/views.py
@@ -59,7 +59,6 @@ def post_remove(request, pk):
 	post.delete()
 	return redirect('blog.views.post_list')
 
-@login_required
 def add_comment_to_post(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
